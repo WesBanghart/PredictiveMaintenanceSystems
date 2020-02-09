ad222358b2919cd94178803c4334ccdc79c8dc42
WesBanghart <Wbang98@gmail.com>
2020-02-09 10:40:55 -0800

    Added Models. Added API Session Context. Added User Controller.

---
+++ PredictiveMaintenanceSystems/SystemAPI/Models/MLModelModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SystemAPI.Models
+{
+    public class MlModelModel
+    {
+        [Key]
+        public string MlModelId { get; set; }
+        public string UserId { get; set; }
+        public string TenantId { get; set; }
+        public string ModelName { get; set; }
+        public string Configuration { get; set; }
+        public byte[] File { get; set; }
+        public DateTime? LastUpdated { get; set; }
+    }
+}
---
+++ PredictiveMaintenanceSystems/SystemAPI/Models/SessionModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SystemAPI.Models
+{
+    public class SessionModel
+    {
+        
+        public float Version { get; set; }
+        //TODO Add prop for Auth token
+    }
+}
